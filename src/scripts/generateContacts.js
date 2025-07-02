3d879f336ffd419bb0a08d497d3c51886a58b44d
alina <alinkash2811@gmail.com>
2025-07-02 11:15:14 +0200

    done

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,19 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+    
+const generateContacts = async (number) => {
+    const contacts = await readContacts();
+    
+    const newContacts = [];
+    for (let i = 0; i < number; i++) {
+        const contact = createFakeContact();
+        newContacts.push(contact);
+    }
+
+    const updatedContacts = [...contacts, ...newContacts];
+
+    return writeContacts(updatedContacts);
+};
 
 generateContacts(5);
